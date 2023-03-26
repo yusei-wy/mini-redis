8045051bca387fcb473d6aa69b4091bcf03b93d6
yusei-wy <yusei.kasa@gmail.com>
2023-03-26 22:50:16 +0900

    add: 読み書きサンプル

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    assert_eq!(result, Some("world".into()));
+
+    Ok(())
 }
