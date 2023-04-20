49673c3ec54f701fd4220964eabf0427aa532e79
yusei-wy <yusei.kasa@gmail.com>
2023-04-20 09:45:38 +0900

    change: HashMap をスレッド間で共有するように

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,50 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+
+        // ハッシュマップへのハンドルを複製する
+        let db = db.clone();
+
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
 
-    let mut db = HashMap::new();
-
+    // `mini-redis` が提供するコネクションによって、ソケットからくるフレームをパースする
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // `Fame::Bluk` はデータが Bytes 型であることを期待する
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
@@ -38,6 +52,7 @@ async fn process(socket: TcpStream) {
             cmd => panic!("unimplemented {:?}", cmd),
         };
 
+        // クライアントへのレスポンスを書き込む
         connection.write_frame(&response).await.unwrap();
     }
 }
