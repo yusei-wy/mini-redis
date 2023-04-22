1d6be0b2c2f118221d3aad4446186961ac22630d
yusei-wy <yusei.kasa@gmail.com>
2023-04-22 16:18:37 +0900

    add: io example

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bytes::Bytes;
 use mini_redis::client;
 use tokio::sync::{mpsc, oneshot};
 
+/// Multiple different commands are multiplexed over a single channel.
 #[derive(Debug)]
 enum Command {
     Get {
@@ -10,38 +11,43 @@ enum Command {
     },
     Set {
         key: String,
-        val: Vec<u8>,
+        val: Bytes,
         resp: Responder<()>,
     },
 }
 
+/// Provided by the requester and used by the manager task to send the command
+/// response back to the requester.
 type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
     let (tx, mut rx) = mpsc::channel(32);
-    // Clone a `tx` handler for the second f
+    // Clone a `tx` handle for the second f
     let tx2 = tx.clone();
 
     let manager = tokio::spawn(async move {
+        // Open a connection to the mini-redis address.
         let mut client = client::connect("127.0.0.1:6379").await.unwrap();
 
         while let Some(cmd) = rx.recv().await {
-            use Command::*;
-
             match cmd {
-                Get { key, resp } => {
+                Command::Get { key, resp } => {
                     let res = client.get(&key).await;
+                    // Ignore errors
                     let _ = resp.send(res);
                 }
-                Set { key, val, resp } => {
-                    let res = client.set(&key, val.into()).await;
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    // Ignore errors
                     let _ = resp.send(res);
                 }
             }
         }
     });
 
+    // Spawn two tasks, one setting a value and other querying for key that was
+    // set.
     let t1 = tokio::spawn(async move {
         let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
@@ -49,11 +55,13 @@ async fn main() {
             resp: resp_tx,
         };
 
+        // Send the GET request
         if tx.send(cmd).await.is_err() {
             eprintln!("connection task shutdown");
             return;
-        };
+        }
 
+        // Await the response
         let res = resp_rx.await;
         println!("GOT (Get) = {:?}", res);
     });
@@ -66,11 +74,13 @@ async fn main() {
             resp: resp_tx,
         };
 
+        // Send the SET request
         if tx2.send(cmd).await.is_err() {
             eprintln!("connection task shutdown");
             return;
         }
 
+        // Await the response
         let res = resp_rx.await;
         println!("GOT (Set) = {:?}", res);
     });
